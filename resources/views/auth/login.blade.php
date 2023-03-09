2ed31d1aa52fcac38568fc6491d205e9cfc6fb51
auvagha-jb <jerry.auvagha@gmail.com>
2023-03-09 22:23:13 +0300

    Fix for auth pages - Added login functionality - Removed default bg image on login, register pages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,44 +8,49 @@
                 <div class="card">
                     <div class="row">
                         <div class="col-md-4 pr-md-0">
-                            <div class="auth-left-wrapper" style="background-image: url({{ url('https://via.placeholder.com/219x452') }})">
+                            <div class="auth-left-wrapper bg-primary">
                             </div>
                         </div>
                         <div class="col-md-8 pl-md-0">
-                            <div class="auth-form-wrapper px-4 py-5">
-                                <a href="#" class="noble-ui-logo d-block mb-2">Noble<span>UI</span></a>
-                                <h5 class="text-muted font-weight-normal mb-4">Welcome back! Log in to your account.</h5>
-                                    @csrf
+                            <form action="{{ route('login') }}" method="post">
+                                @csrf
+                                <div class="auth-form-wrapper px-4 py-5">
+                                    <a href="#" class="noble-ui-logo d-block mb-2">Noble<span>UI</span></a>
+                                    <h5 class="text-muted font-weight-normal mb-4">Welcome back! Log in to your account.</h5>
+
+                                    <!-- Session Status -->
+                                    <x-auth-session-status class="mb-4" :status="session('status')" />
+
+                                    <!-- Validation Errors -->
+                                    <x-auth-validation-errors class="mb-4" :errors="$errors" />
+
                                     <div class="form-group">
                                         <label for="email">Email address</label>
-                                        <input type="email" class="form-control" id="email" placeholder="Email">
+                                        <input type="email" class="form-control" name="email" id="email" placeholder="Email">
                                     </div>
                                     <div class="form-group">
                                         <label for="password">Password</label>
-                                        <input type="password" class="form-control" id="password" autocomplete="password"
+                                        <input type="password" class="form-control" name="password" id="password" autocomplete="password"
                                             placeholder="Password">
                                     </div>
                                     <div class="form-check form-check-flat form-check-primary">
                                         <label class="form-check-label">
-                                            <input type="checkbox" class="form-check-input">
+                                            <input type="checkbox" class="form-check-input" name="remember">
                                             Remember me
                                         </label>
                                     </div>
                                     <div class="mt-3">
-                                        <a href="{{ url('/') }}" class="btn btn-primary mr-2 mb-2 mb-md-0">Login</a>
-                                        <button type="button" class="btn btn-outline-primary btn-icon-text mb-2 mb-md-0">
-                                            <i class="btn-icon-prepend" data-feather="twitter"></i>
-                                            Login with twitter
+                                        <button type="submit" class="btn btn-outline-primary btn-icon-text mb-2 mb-md-0">
+                                            Login
                                         </button>
                                     </div>
-                                    <a href="{{ url('/auth/register') }}" class="d-block mt-3 text-muted">Not a user? Sign up</a>
-                                </form>
-                            </div>
+                                    <a href="{{ route('register') }}" class="d-block mt-3 text-muted">Not a user? Sign up</a>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-
     </div>
 @endsection
